ecb9bc21a38ec51559d5a86019a6fcf8e10ae839
Forever Young <dy9040@gmail.com>
2022-06-08 16:04:44 -0500

    fixed some code then added user routes.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 router.use((req, res) => {
-    res.status(404).send({ '<h1>404 Not Found</h1>': 'The page you are looking for does not exist.' );
+    res.status(404).send( '<h1>404 Not Found</h1>' | 'The page you are looking for does not exist.' );
 });
 
 module.exports = router;
